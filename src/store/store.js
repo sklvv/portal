c7282233bdf6286e03c03c6d12efebb6bb63524b
sklvv <iisokolov1@gmail.com>
2024-12-19 13:42:27 +0500

    fix: typo in imports

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import header from "../pages/header/HeaderSlice";
-import modal from '../elements/Modal/ModalSlice'
+import modal from "../elements/Modal/ModalSlice";
 import sidemenu from "../pages/sidebar/SideMenuSlice";
-import phonebook from "../pages/phoneBook/PhoneBookSlice"
-import transport from "../pages/transport/TransportSlice"
-import userAdmin from "../pages/userAdmin/UserAdminSlice"
-
+import phonebook from "../pages/PhoneBook/PhoneBookSlice";
+import transport from "../pages/transport/TransportSlice";
+import userAdmin from "../pages/userAdmin/UserAdminSlice";
 
 const store = configureStore({
-  reducer: {header, modal, sidemenu, phonebook, transport, userAdmin},
-  middleware: getDefaultMiddleware => getDefaultMiddleware(
-      {
-        serializableCheck: false,
-      }
-  ),
-  devTools: process.env.NODE_ENV !== 'production',
-
-})
+	reducer: { header, modal, sidemenu, phonebook, transport, userAdmin },
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}),
+	devTools: process.env.NODE_ENV !== "production",
+});
 
 export default store;
-
