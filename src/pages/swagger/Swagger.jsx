49161c6f6a839bf7a257954436a5bb7c0f2af35d
sklvv <iisokolov1@gmail.com>
2025-01-14 15:00:30 +0500

    feat: integrate ProjectsBar and RequestInfoBox components into Swagger page

--- src/pages/swagger/Swagger.jsx
+++ src/pages/swagger/Swagger.jsx
@@ -1,11 +1,15 @@
-import './swagger.scss'
+import { Box } from "@mui/material";
+import ProjectsBar from "./subpages/ProjectsBar";
+import RequestInfoBox from "./subpages/RequestInfoBox";
+import "./swagger.scss";
 
 const Swagger = () => {
-    return (
-        <div>
-            1
-        </div>
-    );
+	return (
+		<Box sx={{ display: "flex", flexDirection: "row" }}>
+			<ProjectsBar />
+			<RequestInfoBox />
+		</Box>
+	);
 };
 
 export default Swagger;
